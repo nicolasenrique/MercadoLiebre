f7308cd665de60a8d4e7b878dcddd3ff2e7f838f
nicolasenrique <nicolas.enrique56@gmail.com>
2021-09-22 09:23:20 -0300

    adding project

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-app.listen(process.env.PORT || 3030, () => {
-  console.log("Servidor funcionando");
+
+app.listen(process.env.PORT||3030, ()=>{
+    console.log('Servidor funcionando');
 });
 
-app.get("/", (req, res) => {
-  res.sendFile(dirname + "/views/home.html");
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
 });
 
-app.get("/register", (req, res) => {
-  res.sendFile(dirname + "/views/register.html");
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
 });
 
-app.get("/login", (req, res) => {
-  res.sendFile(__dirname + "/views/login.html");
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
 });
