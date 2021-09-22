b9e59b735c1795699cdcb8d262120cf741a0826e
nicolasenrique <nicolas.enrique56@gmail.com>
2021-09-21 23:47:34 -0300

    adding app

--- app.js
+++ app.js
@@ -6,10 +6,9 @@ const path = require("path");
 const publicPath = path.join(__dirname, "/public");
 app.use(express.static(publicPath));
 
-app.listen(process.env.PORT || 3000, function (){
-  console.log ("Servidor corriendo en el puerto 3000")
-}
-  
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Servidor corriendo en el puerto 3000");
+});
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/views/home.html"));
